42342f80a98487594575979c12dedee50cef53cb
Sheetal Naik <sheetalnaik848@gmail.com>
2025-12-28 17:59:10 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -41,7 +41,7 @@ export const siteConfig: SiteConfig = {
 
 export const navBarConfig: NavBarConfig = {
 	links: [
-		LinkPreset.Home,
+		/*LinkPreset.Home,*/
 		/*LinkPreset.Archive,*/
 		LinkPreset.About,
 		{
