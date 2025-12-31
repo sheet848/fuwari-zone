1715e730b2b33a0a0eed93fb520275d3eb457b41
Sheetal Naik <sheetalnaik848@gmail.com>
2025-12-31 16:20:58 +0530

    Update profile name and bio in config

--- src/config.ts
+++ src/config.ts
@@ -59,8 +59,8 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/profile-image.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	name: "Sheetal Naik",
+	bio: "Frontend Developer",
 	links: [
 		{
 			name: "Gmail",
