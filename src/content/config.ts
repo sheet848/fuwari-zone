a0977a4c56f38996d35a741c943102ec687133aa
Sheetal Naik <sheetalnaik848@gmail.com>
2026-01-05 17:45:43 +0530

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,6 +11,8 @@ const postsCollection = defineCollection({
 		tags: z.array(z.string()).optional().default([]),
 		category: z.string().optional().nullable().default(""),
 		lang: z.string().optional().default(""),
+		github: z.string().url().optional(),
+    	live: z.string().url().optional(),
 
 		/* For internal use */
 		prevTitle: z.string().default(""),
